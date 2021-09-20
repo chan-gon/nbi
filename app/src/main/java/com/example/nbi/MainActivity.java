532ea9ad4223a51ff7da08f3f67f93cf67e3bedf
Charlie <ruggedcafe@gmail.com>
2021-09-20 18:09:22 +0900

    액티비티 팝업 구현중

--- app/src/main/java/com/example/nbi/MainActivity.java
+++ app/src/main/java/com/example/nbi/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.nbi;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.nbi.ui.main.SectionsPagerAdapter;
@@ -37,9 +36,21 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
+                finish();
+
             }
         });
     }
+    //버튼
+    public void mOnPopupClick(View v){
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(this, AddActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivityForResult(intent, 1);
+    }
+
+
 }
